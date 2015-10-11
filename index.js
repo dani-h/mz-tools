1b52463ff2d2c07ebc1ad80cc9ee66f7f26a8912
Dani Hodovic <danihgit@gmail.com>
2015-10-11 19:27:16 +0200

    Fixed bugs, added current_level field
    Removed `insert or ignore` from the database wrappers as it risks to
    surpress errors that may be important to check. Instead we need to check
    insertions that already exist before adding them.
    
    Added the current_level field as skills train slower as the number of
    balls increase. May be important for prediction overall training speed.

--- index.js
+++ index.js
@@ -10,28 +10,38 @@ storage.init(localutil.DBFILE).then(db => {
   requests.getCookies()
     .then(cookies => requests.getAllPlayersHTML(cookies))
     .then(playersHTML => {
+      //Declare the *date* here, so that all the reports share the same date
       let todaysDate = storage.getMidnightDate();
       let playerIDs = parsers.parseAllPlayerIDS(playersHTML);
+
       playerIDs.forEach(id => {
         let playerContainerHTML = parsers.parsePlayerContainer(playersHTML, id);
         let playerData = parsers.parsePlayerFromHTML(playerContainerHTML);
         let reportData = parsers.parseDailyTraining(id, playerContainerHTML);
 
-        storage.insertOrIgnorePlayer(db, id, playerData.number, playerData.name,
-            playerData.age)
+        storage.getPlayerByID(db, id)
+          .then(player => {
+            if (!player) {
+              return storage.insertPlayer(db, id, playerData.number, playerData.name,
+                playerData.age);
+            } else return player;
+          })
           .then(() => BPromise.all([
             storage.getPlayerByID(db, id),
-            storage.getSkillByName(db, reportData.skill)
+            storage.getSkillByName(db, reportData.skill),
+            storage.getMaxTrHistoryDateForPlayerID(db, id)
           ]))
-          .spread((player, skill) => {
-            let date = new Date();
-            //If it's a Sunday its weekly training report, so exclude that
-            if (date.getDay !== 0) {
-              storage.insertOrIgnoreReport(db, player.id, skill.id, todaysDate,
-                reportData.progress, reportData.ballGained, reportData.rested,
-                reportData.boost);
+          .spread((player, skill, maxTrHistoryDate) => {
+            //Dont insert if we already have an entry for today. Do this on a player-by-player
+            //basis as purchased players may have no report for today
+            if (maxTrHistoryDate !== todaysDate.getTime()) {
+              storage.insertReport(db, player.id, skill.id, todaysDate, reportData.progress,
+                reportData.ballGained, reportData.rested, reportData.boost,
+                reportData.currentLevel
+              );
             }
           });
+
       });
     });
 });
