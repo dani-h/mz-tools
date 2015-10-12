4bda1e88f60a10797c00b840a5c33b2d3ad3ea70
Dani Hodovic <danihgit@gmail.com>
2015-10-12 20:28:34 +0200

    Added cli tools
    Moved scraping logic to scrapers.js and added yargs for arg parsing.

--- index.js
+++ index.js
@@ -1,55 +1,32 @@
 "use strict";
 
-let BPromise = require('bluebird');
-let storage = require('./lib/storage.js');
-let requests = require("./lib/requests");
-let parsers = require("./lib/parsers");
-let localutil = require('./lib/util.js');
 let winston = require('winston');
+let yargs = require('yargs');
+
+let localutil = require('./lib/util');
+let storage = require('./lib/storage');
+let scrapers = require('./lib/scrapers');
 
 winston.add(winston.transports.File, {
   filename: localutil.LOGFILE
 });
 
-winston.info('Running scripts.');
 process.on('uncaughtException', (err) => winston.warn(err));
 
-storage.init(localutil.DBFILE).then(db => {
-  requests.getCookies()
-    .then(cookies => requests.getAllPlayersHTML(cookies))
-    .then(playersHTML => {
-      //Declare the *date* here, so that all the reports share the same date
-      let todaysDate = storage.getMidnightDate();
-      let playerIDs = parsers.parseAllPlayerIDS(playersHTML);
+let args = yargs
+  .option('f', {
+    alias: 'fetch',
+    demand: false,
+    default: false,
+    describe: 'Fetch the daily training report',
+    type: 'boolean'
+  })
+  .help('help')
+  .argv;
 
-      playerIDs.forEach(id => {
-        let playerContainerHTML = parsers.parsePlayerContainer(playersHTML, id);
-        let playerData = parsers.parsePlayerFromHTML(playerContainerHTML);
-        let reportData = parsers.parseDailyTraining(id, playerContainerHTML);
 
-        storage.getPlayerByID(db, id)
-          .then(player => {
-            if (!player) {
-              return storage.insertPlayer(db, id, playerData.number, playerData.name,
-                playerData.age);
-            } else return player;
-          })
-          .then(() => BPromise.all([
-            storage.getPlayerByID(db, id),
-            storage.getSkillByName(db, reportData.skill),
-            storage.getMaxTrHistoryDateForPlayerID(db, id)
-          ]))
-          .spread((player, skill, maxTrHistoryDate) => {
-            //Dont insert if we already have an entry for today. Do this on a player-by-player
-            //basis as purchased players may have no report for today
-            if (maxTrHistoryDate !== todaysDate.getTime()) {
-              storage.insertReport(db, player.id, skill.id, todaysDate, reportData.progress,
-                reportData.ballGained, reportData.rested, reportData.boost,
-                reportData.currentLevel
-              );
-            }
-          });
-
-      });
-    });
-});
+if (args.fetch) {
+  storage.createDb(localutil.DBFILE).then(db => {
+    scrapers.parseDailyTraining(db);
+  });
+}
